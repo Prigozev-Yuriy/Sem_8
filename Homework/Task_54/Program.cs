76983782e26cd197628cff67a92e30df617be4e1
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-07-24 16:21:05 +0300

    Task_56

--- Homework/Task_54/Program.cs
+++ Homework/Task_54/Program.cs
@@ -12,61 +12,61 @@
 using System.ComponentModel.DataAnnotations;
 using System.Numerics;
 
-int[,] CreateMatrix(int m, int n, int min, int max)
-{
-    int[,] matrix = new int[m, n];
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(min, max + 1);
-        }
-    }
-    return matrix;
-}
+// int[,] CreateMatrix(int m, int n, int min, int max)
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
 
-int[,] ChangeMatrix(int[,] matrix)
-{
-    int temp;
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int k = 0; k < matrix.GetLength(1); k++)
-        {
-            for (int g = 0; g < matrix.GetLength(1) - 1; g++)
-            {
-                if (matrix[i, g] < matrix[i, g + 1])
-                {
-                    temp = matrix[i, g];
-                    matrix[i, g] = matrix[i, g + 1];
-                    matrix[i, g + 1] = temp;
-                }
+// int[,] ChangeMatrix(int[,] matrix)
+// {
+//     int temp;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int k = 0; k < matrix.GetLength(1); k++)
+//         {
+//             for (int g = 0; g < matrix.GetLength(1) - 1; g++)
+//             {
+//                 if (matrix[i, g] < matrix[i, g + 1])
+//                 {
+//                     temp = matrix[i, g];
+//                     matrix[i, g] = matrix[i, g + 1];
+//                     matrix[i, g + 1] = temp;
+//                 }
 
-            }
-        }
-    }
+//             }
+//         }
+//     }
 
-    return matrix;
-}
+//     return matrix;
+// }
 
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write(matrix[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
 
-Console.Write("Введите количество строк: ");
-int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите количество cтолбцов: ");
-int columns = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество cтолбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
 
-int[,] matr = CreateMatrix(rows, columns, 0, 10);
-int[,] matrix = ChangeMatrix(matr);
-PrintMatrix(matrix);
+// int[,] matr = CreateMatrix(rows, columns, 0, 10);
+// int[,] matrix = ChangeMatrix(matr);
+// PrintMatrix(matrix);
---
+++ Homework/Task_56/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[] SumRows(int[,] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+      for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array [i] = array [i] + matrix[i, j];
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество cтолбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] matr = CreateMatrix(rows, columns, min, max);
+
+int[] sum = SumRows(matr);
+
+PrintMatrix(matr);
+
+Console.WriteLine($"{Array.IndexOf(sum, sum.Min())+1} строка с наименьшей суммой элементов. Сумма элементов равна {sum[Array.IndexOf(sum, sum.Min())]}");
+
